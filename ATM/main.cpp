9fb1aa8d4f2b97460160a96193af75d819ac28d9
Pepe <Pepe@macbooks-mbp.att.net>
2017-08-29 16:42:42 -0400

    Initial Commit

---
+++ ATM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ATM
+//
+//  Created by Pepe on 8/29/17.
+//  Copyright © 2017 Pepe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
